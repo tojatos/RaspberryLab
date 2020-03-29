da7ae05d08bf344a71aacc58f05462a38132bf39
tojatos <tojatos@gmail.com>
2020-03-29 15:50:01 +0200

    most data functions and db done

--- src/app/data.py
+++ src/app/data.py
@@ -1,7 +1,41 @@
 import sqlite3
+from time import gmtime, strftime
 
 conn = sqlite3.connect("app/database/database.db")
 c = conn.cursor()
 
+def fetchall(sql):
+    return c.execute(sql).fetchall()
+
 def get_employees():
-    return c.execute("SELECT * FROM Employee").fetchall();
+    return fetchall("SELECT * FROM Employee")
+
+def get_cards():
+    return fetchall("SELECT * FROM Card")
+
+def get_card_readings():
+    return fetchall("SELECT * FROM CardReading")
+
+def get_terminals():
+    return fetchall("SELECT * FROM Terminal")
+
+def insert_employee(name):
+    c.execute("INSERT INTO Employee (name) VALUES (?)", (name,))
+    conn.commit()
+
+def insert_card(rfid):
+    c.execute("INSERT INTO Card (rfid) VALUES (?)", (rfid,))
+    conn.commit()
+
+def insert_terminal(name):
+    c.execute("INSERT INTO Terminal (name) VALUES (?)", (name,))
+    conn.commit()
+
+def update_card_employee(rfid, employee_id=None):
+    c.execute("UPDATE Card SET employeeId = ? WHERE rfid = ?", (employee_id, rfid))
+    conn.commit()
+
+def insert_card_reading(terminal_id, rfid):
+    c.execute("INSERT INTO CardReading (terminalId, rfid, readTime) VALUES (?, ?, ?)",
+              (terminal_id, rfid, strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    conn.commit()
--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -3,4 +3,16 @@ from app import data
 
 def test_employees():
     employees = data.get_employees()
-    print(employees)
+    print("Employees:", employees)
+
+def test_cards():
+    cards = data.get_cards()
+    print("Cards:", cards)
+
+def test_card_readings():
+    card_readings = data.get_card_readings()
+    print("Card readings:", card_readings)
+
+def test_terminals():
+    terminals = data.get_terminals()
+    print("Terminals:", terminals)
