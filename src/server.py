b2c88d10c8bf2be03066349f66014882cfa73893
tojatos <tojatos@gmail.com>
2020-04-08 23:29:41 +0200

    Gui for methods in controller done

--- src/server.py
+++ src/server.py
@@ -4,8 +4,8 @@ from app import controller, data, logger
 import paho.mqtt.client as mqtt
 import tkinter as tk
 import tkinter.messagebox
+from tkinter import simpledialog as sd
 import traceback
-import pdb
 
 def callback_error(self, *args):
     message = 'Generic error:\n\n'
@@ -29,6 +29,48 @@ def print_data(title, data_to_print):
         for j, label in enumerate(labels):
             label.grid(row=i, column=j, sticky='nesw', padx=1, pady=1)
 
+def register_client():
+    client_name = sd.askstring('Input', 'What name to add?', parent=root)
+    if not client_name:
+        return
+    controller.register_client(client_name)
+
+def unregister_client():
+    client_name = sd.askstring('Input', 'What name to remove?', parent=root)
+    if not client_name:
+        return
+    controller.unregister_client(client_name)
+
+def register_employee():
+    employee_name = sd.askstring('Input', 'What name to add?', parent=root)
+    if not employee_name:
+        return
+    controller.register_employee(employee_name)
+
+def unregister_employee():
+    employee_id = sd.askinteger('Input', 'What id to remove?', parent=root)
+    if not employee_id:
+        return
+    controller.unregister_employee(employee_id)
+
+def associate_card_with_employee():
+    rfid = sd.askinteger('Input', 'What rfid to associate?', parent=root)
+    if not rfid:
+        return
+    employee_id = sd.askinteger('Input', 'What employee id to associate?', parent=root)
+    if not employee_id:
+        return
+    controller.associate_card_with_employee(rfid, employee_id)
+
+def disassociate_card_with_employee():
+    rfid = sd.askinteger('Input', 'What rfid to disassociate?', parent=root)
+    if not rfid:
+        return
+    controller.disassociate_card_with_employee(rfid)
+
+def generate_time_report():
+    controller.generate_time_report()
+
 def create_main_root():
     root.title('SERVER')
     root.attributes('-type', 'dialog')
@@ -37,6 +79,13 @@ def create_main_root():
     root['bg'] = 'black'
     label = tk.Label(root, text='Listening to the MQTT', bg='black', fg='white')
     buttons_data = [
+        ('Register client', register_client),
+        ('Unregister client', unregister_client),
+        ('Register employee', register_employee),
+        ('Unregister employee', unregister_employee),
+        ('Associate card with employee', associate_card_with_employee),
+        ('Disassociate card with employee', disassociate_card_with_employee),
+        ('Generate time report', generate_time_report),
         ('Print employees', lambda: print_data('Employees', data.get_employees())),
         ('Print cards', lambda: print_data('Cards', data.get_cards())),
         ('Print terminals', lambda: print_data('Terminals', data.get_terminals())),
@@ -54,7 +103,6 @@ def process_message(client, userdata, message):
     message_decoded = str(message.payload.decode('utf-8'))
     logger.log(f'Message received: {message_decoded}')
     (terminal_id, rfid) = message_decoded.split(' ')
-    #pdb.set_trace()
     controller.register_card_reading(int(terminal_id), int(rfid))
 
 def run_server():
